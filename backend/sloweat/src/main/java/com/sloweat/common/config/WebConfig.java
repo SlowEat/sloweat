7f3795ac3fab9b6e1a03b91e4cff8aa7e9d69a92
ishwang <his8457@naver.com>
2025-07-11 01:27:48 +0900

    fix: CORS 오류관련 Config 설정

--- backend/sloweat/src/main/java/com/sloweat/common/config/WebConfig.java
+++ backend/sloweat/src/main/java/com/sloweat/common/config/WebConfig.java
@@ -2,19 +2,22 @@ package com.sloweat.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  *
  * 설정 파일 예시
  * */
 
-//@Configuration
-public class WebConfig {
-    //@Override
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(false);
+                .allowedOrigins("http://localhost:3000") // React 개발 서버
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 인증 정보(Cookie 등) 허용 시 true
     }
 }
+
