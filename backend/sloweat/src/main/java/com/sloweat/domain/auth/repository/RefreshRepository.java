1607efeb6cea5acc60ddcbb0355274ced399a31f
Jihyun <qlfus010@naver.com>
2025-07-18 15:38:43 +0900

    feat : 로그아웃 구현
    로그아웃 구현

--- backend/sloweat/src/main/java/com/sloweat/domain/auth/repository/RefreshRepository.java
+++ backend/sloweat/src/main/java/com/sloweat/domain/auth/repository/RefreshRepository.java
@@ -1,9 +1,15 @@
 package com.sloweat.domain.auth.repository;
 
 import com.sloweat.domain.auth.entity.Refresh;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshRepository extends JpaRepository<Refresh,Integer> {
     Boolean existsByRefreshToken(String refresh);
+
+    @Transactional
     void deleteByRefreshToken(String refresh);
+
+    @Transactional
+    void deleteByUsername(String localEmail);
 }
